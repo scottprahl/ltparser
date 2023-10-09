a5c83f691bf685a14a2ec234394bda2bc67478be
Scott Prahl <scott@prahls.com>
2023-10-09 08:48:57 -0700

    update strings

--- ltparser/__init__.py
+++ ltparser/__init__.py
@@ -30,8 +30,8 @@ Functions to help raytrace through GRIN lens.
 __version__ = '0.1.0'
 __author__ = 'Scott Prahl'
 __email__ = 'scott.prahl@oit.edu'
-__copyright__ = 'Copyright 2022, Scott Prahl'
+__copyright__ = '2022-23, Scott Prahl'
 __license__ = 'MIT'
-__url__ = 'https://github.com/scottprahl/ltparser.git'
+__url__ = 'https://github.com/scottprahl/ltparser'
 
 from .ltparser import *
